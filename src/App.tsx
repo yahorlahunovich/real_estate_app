63f39639f83cb8e78d60b5f1569c22e47f17aff7
egorlagunovich <egorlagunovich@gmail.com>
2023-10-11 23:34:03 +0200

    pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import Signin from "./assets/pages/Signin";
+import Profile from "./assets/pages/Profile";
+import Signup from "./assets/pages/Signup";
 
 const App = () => {
   return (
-    <div>Hello!</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
