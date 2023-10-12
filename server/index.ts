dfce0ae03efe3206395a98e12caef8a720512965
egorlagunovich <egorlagunovich@gmail.com>
2023-10-12 20:22:57 +0200

    connected to mongodb

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,26 @@
-import express from "express";
+import mongoose from "mongoose";
+
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT;
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err: any) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
