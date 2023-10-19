06552c1faf0c5864e9dee186fc8f9ca8cd9d416a
egorlagunovich <egorlagunovich@gmail.com>
2023-10-19 16:37:34 +0200

    added redux persit

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,13 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./user/userSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
--- client/src/redux/user/userSlice.ts
+++ client/src/redux/user/userSlice.ts
@@ -33,4 +33,4 @@ export const userSlice = createSlice({
 });
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
-export default userSlice.reducer;
+export const userReducer = userSlice.reducer;
