462cb1fdf5ceec0186e31b9b37adbee3f7a2bec7
egorlagunovich <egorlagunovich@gmail.com>
2023-10-21 14:16:26 +0200

    completed update user functionality

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useAppSelector } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import {
   getDownloadURL,
   getStorage,
@@ -7,18 +7,21 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-
-type FormDataType = {
-  avatar: string;
-};
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
-  const { currentUser } = useAppSelector((state) => state.user);
+  const { currentUser, loading } = useAppSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState<any>({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useAppDispatch();
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -47,6 +50,33 @@ const Profile = () => {
       }
     );
   };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
   return (
     <div className="flex flex-col justify-center items-center w-[1200px] m-auto p-24">
       <h1>Profile</h1>
@@ -59,7 +89,7 @@ const Profile = () => {
       />
       <img
         onClick={() => fileRef.current.click()}
-        src={currentUser.avatar}
+        src={formData.avatar ? formData.avatar : currentUser.avatar}
         alt="user's profile image"
         className="w-32 cursor-pointer"
       />
@@ -76,28 +106,37 @@ const Profile = () => {
           ""
         )}
       </p>
-      <form className="flex flex-col gap-3 w-[300px]">
+      <form className="flex flex-col gap-3 w-[300px]" onSubmit={handleSubmit}>
         <input
           type="text"
-          value={currentUser.firstName}
+          id="firstName"
+          defaultValue={currentUser.firstName}
           className="border border-1-black w-full"
+          onChange={handleChange}
         />
         <input
           type="text"
-          value={currentUser.email}
+          id="email"
+          defaultValue={currentUser.email}
           className="border border-1-black w-full"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Password"
           className="border border-1-black w-full"
         />
-        <button className="border border-1-black">Update</button>
+        <button className="border border-1-black" disabled={loading}>
+          {loading ? "Loading..." : "Update"}
+        </button>
       </form>
       <div className="flex flex-row justify-between items-center w-[300px] mt-5">
         <button>Delete Account</button>
         <button>Sign out</button>
       </div>
+      <p className="text-green-400">
+        {updateSuccess ? "User updated successfully!" : ""}
+      </p>
     </div>
   );
 };
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,9 +8,9 @@ export const test = (req, res) => {
   });
 };
 
-export const updateUser = async (req, res) => {
+export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, "You can update only your account!"));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
@@ -28,7 +28,9 @@ export const updateUser = async (req, res) => {
       },
       { new: true }
     );
-    const {password, ...rest} = updatedUser._doc;
+
+    const { password, ...rest } = updatedUser._doc;
+
     res.status(200).json(rest);
   } catch (error) {
     next(error);
